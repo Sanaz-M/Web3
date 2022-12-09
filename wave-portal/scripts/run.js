df7b1208da6f2450abe9f939f4030b98bcc32931
Sanaz M <sanazmohammaddi@gmail.com>
2022-12-09 11:18:01 +0100

    Count the waves

--- wave-portal/scripts/run.js
+++ wave-portal/scripts/run.js
@@ -2,6 +2,8 @@
 // Imitating the blockchain environment to test
 
 const main = async () => {
+  //the wallet address of contract owner and a random wallet address
+  const [owner, randomPerson] = await hre.ethers.getSigners();
 
   //This will actually compile our contract and generate the necessary files we need to work with our contract
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
@@ -12,6 +14,25 @@ const main = async () => {
 
   //waveContract.address  will basically give us the address of the deployed contract. 
   console.log("Contract deployed to:", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await waveContract.getTotalWaves();
+
+  // someone is waving :)
+  const waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  await waveContract.getTotalWaves();
 };
 
-main();
+const runMain = async() => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
