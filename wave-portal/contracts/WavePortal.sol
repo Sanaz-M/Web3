6989c0d70b0a164f678a58ecd6be432240e5ee34
Sanaz M <sanazmohammaddi@gmail.com>
2022-12-09 11:15:45 +0100

    Count the waves
    Add 2 functions to contracts to check the wallet and count the waves.
    Also, change the run.js to save the new data from our contract. Now it can display the waver address, owner address, and how many times we received waves.

--- wave-portal/contracts/WavePortal.sol
+++ wave-portal/contracts/WavePortal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
     constructor() {
         console.log("Heyyyyyyy, I am a smart contract. I was born to change your world!!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        // msg.sender is the wallet address of the person who called the function. 
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns(uint256) {
+         console.log("We have %d total waves!", totalWaves);
+         return totalWaves;
     }
 }
